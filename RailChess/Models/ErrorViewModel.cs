176187d1cebe638c3444bb5c6d178787a32f0b2c
Au <18971501210@189.cn>
2024-02-04 19:25:35 +0800

    添加项目文件。

---
+++ RailChess/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace RailChess.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ RailChess/Services/Authentication.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+
+namespace RailChess.Services
+{
+    public static class Authentication
+    {
+        public static IServiceCollection AddJwtService(this IServiceCollection services, IConfiguration config)
+        {
+            string domain = config["Jwt:Domain"] ?? throw new Exception("未找到配置项Jwt:Domain");
+            string jwtKey = config["Jwt:SecretKey"] ?? throw new Exception("未找到配置项Jwt:SecretKey");
+
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+                .AddJwtBearer(options => {
+                    options.TokenValidationParameters = new TokenValidationParameters
+                    {
+                        ValidateIssuer = true,//是否验证Issuer
+                        ValidateAudience = true,//是否验证Audience
+                        ValidateLifetime = true,//是否验证失效时间
+                        ClockSkew = TimeSpan.FromSeconds(30),
+                        ValidateIssuerSigningKey = true,//是否验证SecurityKey
+                        ValidAudience = domain,//Audience
+                        ValidIssuer = domain,//Issuer，这两项和前面签发jwt的设置一致
+                        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwtKey))//拿到SecurityKey
+                    };
+                });
+            return services;
+        }
+    }
+}
