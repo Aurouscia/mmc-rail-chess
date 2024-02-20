d5263dcad8636ece9ee19a0a2b4e1fa79e283116
Au <18971501210@189.cn>
2024-02-20 14:02:54 +0800

    首页git栏从svg改为html的

--- RailChessFront/railchess-frontend/src/main.ts
+++ RailChessFront/railchess-frontend/src/main.ts
@@ -6,6 +6,10 @@ import { routes } from './routes'
 
 const router = createRouter({
 history: createWebHashHistory(),routes})
-
+router.afterEach((to,from)=>{
+    if(to.path == "/" && from.path != "/"){
+        location.reload();//要不然主页的那个git展示框就没了
+    }
+})
 export{router}
 createApp(App).use(router).mount('#app')
--- RailChessFront/railchess-frontend/src/pages/HomePage.vue
+++ RailChessFront/railchess-frontend/src/pages/HomePage.vue
@@ -1,36 +1,38 @@
 <script setup lang="ts">
-
 </script>
 
 <template>
 <h1>欢迎来到轨交棋</h1>
-<div>
-    <p>
-        轨交棋是一款基于地铁线路图的棋类游戏，玩家需要在地铁图上沿着线路移动，
-        尽可能多的"占领"车站，并堵截其他玩家的行动，最终获得最高分数。
-    </p>
-    <p>
-        本软件为轨交棋的第二代线上游戏实现，相比第一代有更友好的UI和动画，也更便于部署和维护。
-    </p>
-</div>
-<div class="gitee">
-    <img src="https://gitee.com/au114514/mmc-rail-chess/widgets/widget_card.svg?colors=4183c4,ffffff,ffffff,e3e9ed,666666,9b9b9b"/>
-</div>
-<div>
-    <p>感谢mmc及其同学创造了本游戏玩法，感谢mtp、四氨合铜离子、等人的支持。</p>
-</div>
+    <div>
+        <p>
+            轨交棋是一款基于地铁线路图的棋类游戏，玩家需要在地铁图上沿着线路移动，
+            尽可能多的"占领"车站，并堵截其他玩家的行动，最终获得最高分数。
+        </p>
+        <p>
+            本软件为轨交棋的第二代线上游戏实现，相比第一代有更友好的UI和动画，也更便于部署和维护。
+        </p>
+    </div>
+    
+    <div class="gitee">
+        <div id="osc-gitee-widget-tag"></div>
+    </div>
+
+    <div>
+        <p>感谢mmc及其同学创造了本游戏玩法，感谢mtp、四氨合铜离子、等人的支持。</p>
+    </div>
 </template>
 
+<style>
+.osc_pro_color {color: #4183c4 !important;}
+.osc_panel_color {background-color: #ffffff !important;}
+.osc_background_color {background-color: #ffffff !important;}
+.osc_border_color {border-color: #e3e9ed !important;}
+.osc_desc_color {color: #666666 !important;}
+.osc_link_color * {color: #9b9b9b !important;}
+</style>
 <style scoped>
 .gitee{
-    margin: 30px 0px 30px 0px;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-}
-img{
-    max-width: 100%;
-    object-fit: contain;
+    margin: 20px 0px 20px 0px;
 }
 h1{
     text-align: center;
