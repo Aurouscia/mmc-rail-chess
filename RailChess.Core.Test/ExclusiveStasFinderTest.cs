41c35ff3080d905ccdd882cb964eb74db7f926b4
Au <18971501210@189.cn>
2024-02-20 15:29:00 +0800

    修复“其他人不可到达车站”计算异常的bug

--- RailChess.Core.Test/ExclusiveStasFinderTest.cs
+++ RailChess.Core.Test/ExclusiveStasFinderTest.cs
@@ -119,5 +119,54 @@ namespace RailChess.Core.Test
             var paths3 = _finder.FindExclusiveStas(graph, 2).ToList();
             CollectionAssert.AreEquivalent(new List<int>() { 6 }, paths3);
         }
+
+        [TestMethod]
+        public void PlayersOverlapped()
+        {
+            // 1-2-3
+            //   | |
+            // 4-5-6
+            // | | |
+            // 7-8-9-10
+            var sta1 = new Sta(1, 0);
+            var sta2 = new Sta(2, 1);
+            var sta3 = new Sta(3, 3);
+            var sta4 = new Sta(4, 0);
+            var sta5 = new Sta(5, 0);
+            var sta6 = new Sta(6, 3); 
+            // 玩家1在位置2，为玩家1计算Exclusive车站时，
+            // 位置6被玩家3占了，导致位于7的2号玩家无法到达3，但可以到达9（加入了"otherReachable" HashSet），
+            // 导致位于10的3号玩家一出门就被判定为无法占领更多点，结束计算，没算入其能到达的3号位置
+            var sta7 = new Sta(7, 2);
+            var sta8 = new Sta(8, 0);
+            var sta9 = new Sta(9, 0);
+            var sta10 = new Sta(10, 3);
+            Dictionary<int, int> playerPosition = new()
+            {
+                { 1, 2 }, { 2, 7 }, { 3, 10 }
+            };
+            Graph graph = new(new(){
+                sta1, sta2, sta3, sta4, sta5, sta6, sta7, sta8, sta9, sta10
+            }, playerPosition);
+            sta1.TwowayConnect(sta2);
+            sta2.TwowayConnect(sta3);
+
+            sta2.TwowayConnect(sta5);
+            sta3.TwowayConnect(sta6);
+
+            sta4.TwowayConnect(sta5);
+            sta5.TwowayConnect(sta6);
+
+            sta4.TwowayConnect(sta7);
+            sta5.TwowayConnect(sta8);
+            sta6.TwowayConnect(sta9);
+
+            sta7.TwowayConnect(sta8);
+            sta8.TwowayConnect(sta9);
+            sta9.TwowayConnect(sta10);
+
+            var res = _finder.FindExclusiveStas(graph, 1).ToList();
+            CollectionAssert.AreEquivalent(new List<int>() { 1, 2 }, res);
+        }
     }
 }
--- RailChess.Core/ExclusiveStasFinder.cs
+++ RailChess.Core/ExclusiveStasFinder.cs
@@ -8,54 +8,42 @@ namespace RailChess.Core
     {
         public IEnumerable<int> FindExclusiveStas(Graph graph, int userId)
         {
-            var allStas = graph.Stations.ConvertAll(x => x.Id);
-            HashSet<int> otherReachable = new();
+            var allStas = graph.Stations;
+            var othersReachable = new HashSet<int>();
             foreach(var p in graph.UserPosition)
             {
-                var user = p.Key;
+                var he = p.Key;
                 var from = p.Value;
-                if (user == userId) continue;
-                List<Sta> active = new();
-                List<List<Sta>> paths = new();
-                
+                if (he == userId) continue;
+                HashSet<int> capturedByOthers = new(allStas.Where(x => x.Owner != 0 && x.Owner != he).Select(x => x.Id));
+                HashSet<int> hisReachable = new();
+                Queue<Sta> active = new();
                 var startPoint = graph.Stations.Find(x => x.Id == from) ?? throw new Exception("算路异常:找不到指定起始点");
-                active.Add(startPoint);
-                paths.Add(new() { startPoint });
-                otherReachable.Add(startPoint.Id);
-                var step = 0;
+
+                active.Enqueue(startPoint);
+                hisReachable.Add(startPoint.Id);
+
                 while (true)
                 {
-                    int originalCount = otherReachable.Count;
-
-                    foreach (var a in active)
+                    var a = active.Dequeue();
+                    foreach (var n in a.Neighbors)
                     {
-                        var aPath = paths.Find(x => x.Count >= 1 && x.Last().Id == a.Id);
-                        if (aPath is null) continue;
-                        foreach (var n in a.Neighbors)
-                        {
-                            if (n.Owner != 0 && n.Owner != user) continue;//不是自己的/空的就不能往这走
-                            if (aPath.Count >= 2)
-                            {
-                                var lastButOne = aPath[^2];
-                                if (lastButOne.Id == n.Id) continue;
-                            }
-                            otherReachable.Add(n.Id);
-                            List<Sta> newPath = new(aPath) { n };
-                            paths.Add(newPath);
-                        }
+                        if (capturedByOthers.Contains(n.Id))
+                            continue;//不能往其他人占了的点走
+                        if (hisReachable.Contains(n.Id))
+                            continue;//不能往回走
+                        hisReachable.Add(n.Id);
+                        active.Enqueue(n);
                     }
-                    step++;
-                    paths.RemoveAll(x => x.Count != step + 1);
-                    if (paths.Count == 0) break;
-                    paths = paths.DistinctBy(x => x.Last().Id).ToList();//去除当前终点相同的路径
-                    active = paths.ConvertAll(x => x.Last());
+                    if (active.Count == 0) break;
+                }
 
-                    if (originalCount == otherReachable.Count)
-                        break;
+                foreach(var r in hisReachable)
+                {
+                    othersReachable.Add(r);
                 }
             }
-            allStas.RemoveAll(otherReachable.Contains);
-            return allStas;
+            return allStas.ConvertAll(x=>x.Id).Except(othersReachable);
         }
     }
 }
