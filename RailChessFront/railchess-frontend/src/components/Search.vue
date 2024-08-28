31edc580fc60bfa5e3c948853faa9515e0358cba
Au <18971501210@189.cn>
2024-08-28 23:31:31 +0800

    优化搜索框

--- RailChessFront/railchess-frontend/src/components/Search.vue
+++ RailChessFront/railchess-frontend/src/components/Search.vue
@@ -1,19 +1,24 @@
 <script setup lang="ts">
-import { onMounted, ref } from 'vue';
+import { onMounted, onUnmounted, ref } from 'vue';
 import { QuickSearchResult, QuickSearchResultItem } from '../models/quickSearch';
 
 const props = defineProps<{
     placeholder?:string,
     allowFreeInput?:boolean|undefined,
     noResultNotice?:string,
-    source:(s:string)=>Promise<QuickSearchResult|undefined>
+    source:(s:string)=>Promise<QuickSearchResult|undefined>,
+    dontClearAfterDone?:boolean,
+    compact?:boolean
 }>()
 
 const doneBtnStatus = ref<boolean>(false);
 const isFreeInput = ref<boolean>(false);
 const searching = ref<string>("");
 const selectedId = ref<number>(0);
+const selectedDesc = ref<string>();
 const cands = ref<QuickSearchResult>();
+const isSearching = ref<boolean>(false);
+const inputing = ref<boolean>(false);
 var timer:number = 0;
 const delay:number = 500;
 function refreshCand(){
@@ -25,17 +30,26 @@ function refreshCand(){
         doneBtnStatus.value = true;
     }
     if(!searching.value){
+        clear()
         return;
     }
+    inputing.value = true;
     window.clearTimeout(timer);
     timer = window.setTimeout(async()=>{
         emits('stopInput',searching.value);
+        inputing.value = false;
+        isSearching.value = true;
+        if(cands.value)
+            cands.value.Items = [];
+        selectCand.value = -1;
         cands.value = await props.source(searching.value);
+        isSearching.value = false;
     },delay)
 }
 function clickCand(c:QuickSearchResultItem){
     searching.value = c.Name;
     selectedId.value = c.Id;
+    selectedDesc.value = c.Desc;
     doneBtnStatus.value = true;
     isFreeInput.value = false;
     if(cands.value){
@@ -44,7 +58,10 @@ function clickCand(c:QuickSearchResultItem){
 }
 function done(){
     if(doneBtnStatus.value){
-        emits('done',searching.value,selectedId.value);
+        emits('done',searching.value, selectedId.value, selectedDesc.value);
+        if(!props.dontClearAfterDone){
+            clear()
+        }
     }
 }
 function clear(){
@@ -52,35 +69,79 @@ function clear(){
     selectedId.value = 0;
     doneBtnStatus.value = false;
     cands.value = undefined;
+    selectCand.value = -1;
+    selectedDesc.value = undefined;
 }
 
 const emits = defineEmits<{
-    (e:'done',value:string,id:number):void
+    (e:'done',value:string,id:number,desc?:string):void
     (e:'stopInput',value:string):void
 }>();
 defineExpose({clear});
 
+
+const selectCand = ref<number>(-1);
+function keyEventHandler(e:KeyboardEvent){
+    if(!cands.value?.Items){
+        return;
+    }
+    const key = e.key;
+    if(key == 'ArrowUp' && selectCand.value>0){
+        selectCand.value -= 1;
+        e.preventDefault();
+    }
+    else if(key == 'ArrowDown' && selectCand.value < cands.value.Items.length - 1){
+        selectCand.value += 1;
+        e.preventDefault();
+    }
+    else if(key == 'Enter'){
+        if(selectCand.value >= 0 && selectCand.value < cands.value.Items.length){
+            const item = cands.value.Items[selectCand.value];
+            if(item){
+                clickCand(item)
+            }
+        }
+        else if(doneBtnStatus.value){
+            done()
+        }
+    }
+}
+
 onMounted(()=>{
+    cands.value = {
+        Items:[]
+    }
+    window.addEventListener('keydown', keyEventHandler);
+})
+onUnmounted(()=>{
+    window.removeEventListener('keydown',keyEventHandler);
 })
 </script>
 
 <template>
-<div class="search">
-    <div class="write">
-        <input v-model="searching" @input="refreshCand" :placeholder="props.placeholder"/>
-        <button class="confirm" :class="{disabled:!doneBtnStatus}" @click="done">确认</button>
-    </div>
-    <div v-if="cands && searching" class="cand">
-        <div class="candItem" v-for="c in cands.Items" @click="clickCand(c)">
-            {{ c.Name }}
-            <div class="desc">{{ c.Desc }}</div>
-        </div>
-            <div class="noResult" v-show="isFreeInput && props.allowFreeInput && props.noResultNotice">{{ props.noResultNotice }}</div>
-            <div class="noResult" v-show="isFreeInput && !props.allowFreeInput && cands.Items.length==0">
-                没有匹配结果
+    <div class="searchOuter">
+        <div class="search" :class="{compact}">
+            <div class="write">
+                <input v-model="searching" @input="refreshCand" :placeholder="props.placeholder" />
+                <button class="confirm" :class="{ disabled: !doneBtnStatus }" @click="done">确认</button>
             </div>
+            <div v-if="cands && searching" class="cand">
+                <div class="candItem" v-for="c,idx in cands.Items" @click="clickCand(c)" :class="{selected:idx == selectCand}">
+                    <div>
+                        {{ c.Name }}
+                        <div class="desc">{{ c.Desc }}</div>
+                    </div>
+                </div>
+                <div class="noResult" v-show="isFreeInput && props.allowFreeInput && props.noResultNotice">{{
+                    props.noResultNotice }}</div>
+                <div class="noResult" v-show="isFreeInput && !props.allowFreeInput && cands.Items.length == 0">
+                    <div v-if="inputing">请继续输入...</div>
+                    <div v-else-if="isSearching">搜索中...</div>
+                    <div v-else>没有匹配结果</div>
+                </div>
+            </div>
+        </div>
     </div>
-</div>
 </template>
 
 <style scoped>
@@ -91,12 +152,31 @@ onMounted(()=>{
 .candItem:hover{
     background-color: #ccc;
 }
+.candItemImage{
+    display: flex;
+    gap:5px;
+    align-items: center;
+    justify-content: left;
+}
+.candItemImage img{
+    height: 25px;
+    max-width: 40px;
+    object-fit: contain;
+}
 .candItem,.noResult{
     border-bottom: 1px solid #aaa;
     padding: 5px;
     transition: 0.5s;
     cursor: pointer;
     user-select: none;
+    transition: 0.1s;
+}
+.candItem.selected{
+    background-color: cornflowerblue;
+    color:white;
+}
+.candItem.selected .desc{
+    color: white;
 }
 .noResult{
     color:#999;
@@ -105,10 +185,12 @@ onMounted(()=>{
 }
 .cand{
     position: absolute;
-    top:30px;
-    left:10px;right:50px;
-    box-shadow: 0px 0px 5px 0px;
+    top:32px;
+    width: 220px;
+    left: 10px;
+    box-shadow: 0px 2px 6px 0px black;
     background-color: white;
+    z-index: 200;
 }
 .write button.disabled{
     background-color: #aaa;
@@ -118,26 +200,46 @@ onMounted(()=>{
     border-radius: 0px 5px 5px 0px;
     padding: 5px;
     margin: 0px;
+    box-sizing: border-box;
+    height: 100%;
     transition: 0.5s;
 }
 .write input{
     border:2px solid cornflowerblue;
     border-radius: 5px 0px 0px 5px;
-    flex-grow: 1;
     padding: 4px;
     margin:0px;
-    height: 19px;
+    height: 100%;
     display: block;
+    box-sizing: border-box;
+    font-size: 16px;
 }
 .write{
     display: flex;
     flex-wrap: nowrap;
     align-items: center;
+    justify-content: center;
     gap:0px;
     position: relative;
     z-index: 50;
+    height: 32px;
 }
 .search{
     position: relative;
+}
+.search.compact{
+    width: 210px;
+}
+.search.compact input{
+    width: 170px;
+}
+.search.compact .cand{
+    width: 150px;
+}
+
+.searchOuter{
+    display: flex;
+    justify-content: center;
+    overflow: visible;
 }
 </style>
