f99b036482bd2b3fc2d89ddfe91d201464888410
Au <18971501210@189.cn>
2024-10-26 00:24:22 +0800

    将topo转graph的方法提取到单独的类种

---
+++ RailChess/Play/Services/Core/CoreGraphConverter.cs
@@ -0,0 +1,47 @@
+﻿using RailChess.GraphDefinition;
+using RailChess.Models.Map;
+
+namespace RailChess.Play.Services.Core
+{
+    public class CoreGraphConverter
+    {
+        public Graph? Convert(RailChessTopo topo)
+        {
+            if (topo.Stations is null || topo.Lines is null)
+                return null;
+            List<Sta> ss = topo.Stations.ConvertAll(x => new Sta(x.Id));
+            topo.Lines.ForEach(line =>
+            {
+                if (line.Stas is not null && line.Stas.Count > 1)
+                {
+                    for (int i = 0; i < line.Stas.Count; i++)
+                    {
+                        int staId = line.Stas[i];
+                        var target = ss.Find(x => x.Id == staId);
+                        if (target is null) continue;
+                        List<int> neighborHere = new(2);
+                        if (i == 0)
+                            neighborHere.Add(line.Stas[1]);//至少有两个站才会进来，1肯定有东西
+                        else if (i == line.Stas.Count - 1)
+                            neighborHere.Add(line.Stas[^2]);
+                        else
+                        {
+                            neighborHere.Add(line.Stas[i - 1]);
+                            neighborHere.Add(line.Stas[i + 1]);
+                        }
+
+                        neighborHere.ForEach(n =>
+                        {
+                            var ns = ss.Find(s => s.Id == n);
+                            if (ns is not null)
+                            {
+                                target.Neighbors.Add(new(line.Id, ns));
+                            }
+                        });
+                    }
+                }
+            });
+            return new Graph(ss);
+        }
+    }
+}
--- RailChess/Play/Services/Core/CoreGraphProvider.cs
+++ RailChess/Play/Services/Core/CoreGraphProvider.cs
@@ -7,13 +7,17 @@ namespace RailChess.Play.Services.Core
     {
         private readonly PlayEventsService _eventsService;
         private readonly PlayToposService _topoService;
+        private readonly CoreGraphConverter _converter;
         private readonly IMemoryCache _cache;
         private readonly ILogger<CoreGraphProvider> _logger;
 
-        public CoreGraphProvider(PlayEventsService eventsService, PlayToposService toposService, IMemoryCache cache, ILogger<CoreGraphProvider> logger) 
+        public CoreGraphProvider(
+            PlayEventsService eventsService, PlayToposService toposService, CoreGraphConverter converter,
+            IMemoryCache cache, ILogger<CoreGraphProvider> logger) 
         {
             _eventsService = eventsService;
             _topoService = toposService;
+            _converter = converter;
             _cache = cache;
             _logger = logger;
         }
@@ -59,41 +63,8 @@ namespace RailChess.Play.Services.Core
         {
             _logger.LogDebug("游戏[{gameId}]_构建核心图", _topoService.GameId);
             var topo = _topoService.OurTopo();
-            if (topo.Lines is null || topo.Stations is null) throw new Exception("地图数据异常(无法构建图)");
-
-            List<Sta> ss = topo.Stations.ConvertAll(x => new Sta(x.Id));
-            topo.Lines.ForEach(line =>
-            {
-                if (line.Stas is not null && line.Stas.Count > 1)
-                {
-                    for (int i = 0; i < line.Stas.Count; i++)
-                    {
-                        int staId = line.Stas[i];
-                        var target = ss.Find(x => x.Id == staId);
-                        if (target is null) continue;
-                        List<int> neighborHere = new(2);
-                        if (i == 0)
-                            neighborHere.Add(line.Stas[1]);//至少有两个站才会进来，1肯定有东西
-                        else if (i == line.Stas.Count - 1)
-                            neighborHere.Add(line.Stas[^2]);
-                        else
-                        {
-                            neighborHere.Add(line.Stas[i - 1]);
-                            neighborHere.Add(line.Stas[i + 1]);
-                        }
-                        
-                        neighborHere.ForEach(n =>
-                        {
-                            var ns = ss.Find(s => s.Id == n);
-                            if (ns is not null)
-                            {
-                                target.Neighbors.Add(new(line.Id, ns));
-                            }
-                        });
-                    }
-                }
-            });
-            return new Graph(ss);
+            var graph = _converter.Convert(topo) ?? throw new Exception("地图数据异常(无法构建图)");
+            return graph;
         }
 
         public Dictionary<int,int> StationDirections()
