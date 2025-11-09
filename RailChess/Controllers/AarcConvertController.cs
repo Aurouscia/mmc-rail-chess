d30c361fec436b3edee143db63836bf7d2ce1574
Au <18971501210@189.cn>
2025-11-09 22:49:57 +0800

    aarc转换器：优化存档存储和发送机制

--- RailChess/Controllers/AarcConvertController.cs
+++ RailChess/Controllers/AarcConvertController.cs
@@ -1,22 +1,28 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System.Net.Http.Headers;
+using System.Net.Mime;
+using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 using RailChess.Utils;
-using RestSharp;
 
 namespace RailChess.Controllers
 {
     public class AarcConvertController: Controller
     {
         private const int saveMaxMb = 10;
-        private const int saveMaxBytes = saveMaxMb * 1000 * 1000;
+        private const int saveMaxBytes = saveMaxMb * 1024 * 1024;
         private static readonly string SaveSizeExceededMsg = $"文件不应超过{saveMaxMb}MB";
-        private const string saveStoreDir = "./Data/Convert/AARC";
+        private const string storeDir = "./Data/Convert/AARC";
+        private const string saveFileName = "aarc.json";
+        private const string reqFileName = "req.json";
+        private const long storeDirMaxBytes = 500 * 1024 * 1024;
 
-        private static bool _converterConfigRead = false;
-        private static RestClient? _client;
+        private static bool _converterConfigRead;
+        private static readonly HttpClient Client = new();
         private static string? _svcUrl;
+        private const string createTaskEndpoint = "/create";
         private readonly ILogger<AarcConvertController> _logger;
+        private static readonly Lock Locker = new();
         public AarcConvertController(IConfiguration config, ILogger<AarcConvertController> logger)
         {
             _logger = logger;
@@ -27,7 +33,6 @@ namespace RailChess.Controllers
             if (url is null)
                 return;
             _svcUrl = url;
-            _client = new RestClient(url);
         }
         
         /// <summary>
@@ -46,10 +51,15 @@ namespace RailChess.Controllers
                 return this.ApiFailedResp("文件应为json");
             if (save.Length > saveMaxBytes)
                 return this.ApiFailedResp(SaveSizeExceededMsg);
-            var dirPath = GetDir()?.FullName;
-            if(dirPath is null)
+            bool okay;
+            lock (Locker)
+            {
+                okay = CleanAndCheckStoreDir();
+            }
+
+            if (!okay)
                 return this.ApiFailedResp("存储失败，请联系管理员");
-            var tempPath = Path.Combine(dirPath, "temp_" + Path.GetRandomFileName());
+            var tempPath = Path.Combine(storeDir , "temp_" + Path.GetRandomFileName());
             var tempInfo = new FileInfo(tempPath);
             FileStream? tempS = null;
             try
@@ -64,7 +74,10 @@ namespace RailChess.Controllers
                 tempS.Seek(0, SeekOrigin.Begin);
                 var md5 = MD5Helper.GetMD5Of(tempS);
                 tempS.Close();
-                var finalPath = Path.Combine(dirPath, Path.ChangeExtension(md5, "json"));
+                var finalDirPath = Path.Combine(storeDir, md5);
+                if(!Directory.Exists(finalDirPath))
+                    Directory.CreateDirectory(finalDirPath);
+                var finalPath = Path.Combine(storeDir, md5, saveFileName);
                 var finalInfo = new FileInfo(finalPath);
                 if (!finalInfo.Exists)
                     tempInfo.MoveTo(finalPath);
@@ -79,69 +92,144 @@ namespace RailChess.Controllers
         }
 
         [HttpPost]
-        public IActionResult CreateTask(string md5, string? configJson)
-        {
-            if (_client is null)
+        public async Task<IActionResult> CreateTask(string md5, string? configJson)
+        {                
+            if (_svcUrl is null)
                 return this.ApiFailedResp("缺少转换器配置");
-            var dirPath = GetDir()?.FullName;
-            if(dirPath is null)
-                return this.ApiFailedResp("存储失败，请联系管理员");
-            var filePath = Path.Combine(dirPath, Path.ChangeExtension(md5, "json"));
-            if (!System.IO.File.Exists(filePath))
-                return this.ApiFailedResp("超时，请刷新页面并重新上传");
-            var fileContent = System.IO.File.ReadAllText(filePath);
-            RestRequest req = new("/create");
-            req.Timeout = TimeSpan.FromSeconds(10);
-            req.AddJsonBody(new
+            var filePath = Path.Combine(storeDir, md5, saveFileName);
+            var reqFilePath = Path.Combine(storeDir, md5, reqFileName);
+            var reqFileInfo = new FileInfo(reqFilePath);
+            lock (Locker)
             {
-                aarc = fileContent,
-                //config = configJson ?? "{}"
-            });
+                if (!reqFileInfo.Exists)
+                {
+                    // 如果req文件不存在则创建
+                    if (!System.IO.File.Exists(filePath))
+                        return this.ApiFailedResp("超时，请刷新页面并重新上传");
+                    try
+                    {
+                        using var reader = new JsonTextReader(System.IO.File.OpenText(filePath))
+                        {
+                            CloseInput = true,
+                            SupportMultipleContent = false
+                        };
+                        using var reqFileStream = System.IO.File.Create(reqFilePath);
+                        using var writer = new JsonTextWriter(new StreamWriter(reqFileStream));
+                        writer.Formatting = Formatting.None;
+                        writer.CloseOutput = true;
+                        writer.WriteStartObject();
+                        writer.WritePropertyName("aarc");
+                        if (!reader.Read() || reader.TokenType != JsonToken.StartObject)
+                            throw new JsonReaderException($"json格式异常");
+                        writer.WriteToken(reader, true);
+                        writer.WriteEndObject();
+                        writer.Flush();
+                    }
+                    catch
+                    {
+                        if (System.IO.File.Exists(reqFilePath))
+                            System.IO.File.Delete(reqFilePath);
+                        return this.ApiFailedResp($"{saveFileName}格式异常");
+                    }
+                }
+                else
+                {
+                    // 如果存在，则设置“上次写”时间，假装新创建了一次（避免被清理）
+                    reqFileInfo.LastWriteTime = DateTime.Now;
+                }
+            }
+
+            await using var reqBodyStream = System.IO.File.OpenRead(reqFilePath);
             try
             {
-                var resp = _client.Post(req);
-                var res = resp.Content;
-                var jObj = JObject.Parse(res ?? "{}");
-                var key = jObj["key"];
-                if (key is not null && key.Type == JTokenType.String)
-                    return this.ApiResp(new { key });
-                return this.ApiFailedResp("aarc转换器接口返回异常");
+                    var content = new StreamContent(reqBodyStream);
+                    content.Headers.ContentType = new MediaTypeHeaderValue(MediaTypeNames.Application.Json);
+                    var url = _svcUrl + createTaskEndpoint;
+                    var resp = await Client.PostAsync(url, content);
+                    resp.EnsureSuccessStatusCode();
+                    var res = resp.Content;
+                    await using var resStream = await res.ReadAsStreamAsync();
+                    await using var reader = new JsonTextReader(new StreamReader(resStream));
+                    var jObj = await JObject.LoadAsync(reader);
+                    var key = jObj["key"];
+                    if (key is not null && key.Type == JTokenType.String)
+                        return this.ApiResp(new { key });
+                    return this.ApiFailedResp("aarc转换器接口返回异常：" + res);
             }
             catch (Exception e)
             {
-                _logger.LogError(e, "aarc转换器接口调用出错");
-                return this.ApiFailedResp("接口异常: " + e.Message);
+                    _logger.LogError(e, "aarc转换器接口调用出错");
+                    try
+                    {
+                        reqFileInfo.Delete();
+                        _logger.LogWarning("aarc转换器已删除出错req文件");
+                    }
+                    catch (Exception e1)
+                    {
+                        _logger.LogError(e1, "aarc转换器删除出错req文件失败");
+                    }
+
+                    return this.ApiFailedResp("aarc转换器接口返回异常: " + e.Message);
             }
         }
 
         public IActionResult GetSvcUrl()
         {
-            return this.ApiResp(new{ svcUrl = _svcUrl});
+            return this.ApiResp(new { svcUrl = _svcUrl });
         }
 
         [NonAction]
-        private static DirectoryInfo? GetDir()
+        private bool CleanAndCheckStoreDir()
         {
-            DirectoryInfo directoryInfo = new(saveStoreDir);
+            DirectoryInfo directoryInfo = new(storeDir);
             if (!directoryInfo.Exists)
                 directoryInfo.Create();
-            var thrs = DateTime.Now.AddMinutes(-30);
-            var oldFiles = directoryInfo
-                .EnumerateFiles()
-                .Where(x => x.CreationTime < thrs)
-                .ToList();
-            foreach(var f in oldFiles)
+            var thrs = DateTime.Now.AddDays(-7);
+            var dirs = directoryInfo.EnumerateDirectories();
+            long sizeSum = 0;
+            foreach (var d in dirs)
             {
-                try 
+                long sizeSumHere = 0;
+                var latest = DateTime.MinValue;
+                var dFiles = d.EnumerateFiles();
+                foreach (var f in dFiles)
+                {
+                    if (f.LastWriteTime > latest)
+                        latest = f.LastWriteTime;
+                    sizeSumHere += f.Length;
+                }
+
+                if (latest >= thrs) continue; // 如果足够新，那没事
+                // 如果小于thrs（不够新）则删除
+                try
+                {
+                    d.Delete(true);
+                    sizeSumHere = 0; // 如果删除失败，不会变0
+                }
+                catch (Exception e)
+                {
+                    _logger.LogWarning("清理失败：{emsg}, {fname}", e.Message, d.Name);
+                }
+
+                sizeSum += sizeSumHere;
+            }
+            var files = directoryInfo.EnumerateFiles();
+            foreach (var f in files)
+            {
+                if(f.LastWriteTime >= thrs) continue;
+                try
                 {
                     f.Delete();
                 }
-                catch { }
+                catch (Exception e)
+                {
+                    _logger.LogWarning("清理失败：{emsg}, {fname}", e.Message, f.Name);
+                }
             }
-            var totalBytes = directoryInfo.EnumerateFiles().Select(x => x.Length).Sum();
-            if (totalBytes > 500 * 1000 * 1000)
-                return null;
-            return directoryInfo;
+
+            if (sizeSum > storeDirMaxBytes)
+                return false; // 超出上限
+            return true;
         }
     }
 }
