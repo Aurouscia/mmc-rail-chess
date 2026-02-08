44c5d31cb5b4090771f07828c3dd848c8eaf755a
Au <18971501210@189.cn>
2026-02-08 16:55:41 +0800

    对局设置移入单独组件

---
+++ RailChessFront/railchess-frontend/src/components/PlayOptions.vue
@@ -0,0 +1,52 @@
+<script setup lang="ts">
+import { storeToRefs } from 'pinia';
+import { usePlayOptionsStore } from '../utils/stores/playOptionsStore';
+
+defineProps<{
+    playback?:boolean
+}>()
+
+const { bgOpacity, staSizeRatio, vacuumStaOpacity, autoSeekInterval, connDisplayMode } = storeToRefs(usePlayOptionsStore())
+</script>
+
+<template>
+    <div class="sideBarOption">
+        <b>背景不透明度：{{ bgOpacity?.toFixed(2) }}</b>
+        <input type="range" v-model.number="bgOpacity" min="0" max="1" step="0.05">
+    </div>
+    <div class="sideBarOption">
+        <b>站点标记尺寸倍率：{{ staSizeRatio?.toFixed(2) }}</b>
+        <input type="range" v-model.number="staSizeRatio" min="0.3" max="1.0" step="0.05">
+        <div style="font-size: 12px;">站点有最小尺寸限制<br/>视角近时调整才看得到效果</div>
+    </div>
+    <div class="sideBarOption">
+        <b>未占站点不透明度：{{ vacuumStaOpacity?.toFixed(2) }}</b>
+        <input type="range" v-model.number="vacuumStaOpacity" min="0.1" max="1.0" step="0.05">
+    </div>
+    <div v-if="playback" class="sideBarOption">
+        <b>自动播放间隔(ms)：{{ autoSeekInterval?.toFixed(0) }}</b>
+        <input type="range" v-model.number="autoSeekInterval" min="1000" max="10000" step="500">
+    </div>
+    <div class="sideBarOption">
+        <b>查看车站连接关系</b>
+        <select v-model="connDisplayMode">
+            <option :value="'none'">关闭</option>
+            <option :value="'anim'">动画</option>
+        </select>
+    </div>
+</template>
+
+<style scoped>
+.sideBarOption input{
+    margin: 0px;
+}
+.sideBarOption{
+    text-align: center;
+    border-bottom: 1px solid #ccc;
+    padding: 10px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    gap: 6px;
+}
+</style>
--- RailChessFront/railchess-frontend/src/pages/Play.vue
+++ RailChessFront/railchess-frontend/src/pages/Play.vue
@@ -18,6 +18,7 @@ import { useRouter } from 'vue-router';
 import { storeToRefs } from 'pinia';
 import { usePlayOptionsStore } from '../utils/stores/playOptionsStore';
 import { useJwtTokenStore } from '../utils/stores/jwtTokenStore';
+import PlayOptions from '../components/PlayOptions.vue';
 
 const router = useRouter()
 const props = defineProps<{
@@ -445,7 +446,7 @@ const sidebarOptions = useTemplateRef('sidebarOptions')
 const msgs = ref<TextMsg[]>([]);
 const frame = useTemplateRef('frame')
 const arena = useTemplateRef('arena')
-const { bgOpacity, staSizeRatio, vacuumStaOpacity, autoSeekInterval, connDisplayMode } = storeToRefs(usePlayOptionsStore())
+const { bgOpacity, staSizeRatio, connDisplayMode } = storeToRefs(usePlayOptionsStore())
 const staSize = ref<number>(0.8)
 function autoStaSize(){
     if(!frame.value || !arena.value){return;}
@@ -684,30 +685,7 @@ watch(props,()=>{
     </div>
 </SideBar>
 <SideBar ref="sidebarOptions">
-    <div class="sideBarOption">
-        <b>背景不透明度：{{ bgOpacity?.toFixed(2) }}</b>
-        <input type="range" v-model.number="bgOpacity" min="0" max="1" step="0.05">
-    </div>
-    <div class="sideBarOption">
-        <b>站点标记尺寸倍率：{{ staSizeRatio?.toFixed(2) }}</b>
-        <input type="range" v-model.number="staSizeRatio" min="0.3" max="1.0" step="0.05">
-        <div style="font-size: 12px;">站点有最小尺寸限制<br/>视角近时调整才看得到效果</div>
-    </div>
-    <div class="sideBarOption">
-        <b>未占站点不透明度：{{ vacuumStaOpacity?.toFixed(2) }}</b>
-        <input type="range" v-model.number="vacuumStaOpacity" min="0.1" max="1.0" step="0.05">
-    </div>
-    <div v-if="playback" class="sideBarOption">
-        <b>自动播放间隔(ms)：{{ autoSeekInterval?.toFixed(0) }}</b>
-        <input type="range" v-model.number="autoSeekInterval" min="1000" max="10000" step="500">
-    </div>
-    <div class="sideBarOption">
-        <b>查看车站连接关系</b>
-        <select v-model="connDisplayMode">
-            <option :value="'none'">关闭</option>
-            <option :value="'anim'">动画</option>
-        </select>
-    </div>
+    <PlayOptions></PlayOptions>
 </SideBar>
 <Timeline v-if="playback" :game-id="gameId" @view-time="t=>sgrc.syncMe(t)"></Timeline>
 </template>
@@ -760,18 +738,6 @@ canvas{
 }
 .randNumIsAB{
     font-size: 22px;
-}
-.sideBarOption input{
-    margin: 0px;
-}
-.sideBarOption{
-    text-align: center;
-    border-bottom: 1px solid #ccc;
-    padding: 10px;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    gap: 6px;
 }
 .sidebarBtns{
     width: 200px;
